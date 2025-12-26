67592242eb6141fedfd65bbdef434eb967a6720e
golamhossendev <golamhossenpolash@gmail.com>
2025-12-26 23:36:53 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 3000, // or your preferred port
+    strictPort: true,
+    allowedHosts: [
+      "cloudpix-fee-btggeqczhac3d6at.switzerlandnorth-01.azurewebsites.net",
+      "localhost",
+    ],
+  },
+});
